8a7cb0e5906568412f7f0f17162aac0934615876
Mateus Pinna Lousa <mateuslousa@gmail.com>
2022-11-20 11:09:29 -0300

    Bot Fazendo Pesquisa de passagem aerea

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -9,86 +9,47 @@ amadeus = Client(
     client_secret='GwXAbyPUziKZC8S9'
 )
 
-
-'''try:
-    response = amadeus.shopping.flight_offers_search.get(
-        originLocationCode='LON',
-        destinationLocationCode='ATH',
-        departureDate='2022-12-01',
-        adults=1)
-    print(response.data[0]['price'])
-except ResponseError as error:
-    print(error)
-
-try:
-    response = amadeus.reference_data.recommended_locations.get(cityCodes='LON', travelerCountryCode='US')
-    #print(response.data)
-except ResponseError as error:
-    print(error)
-
-objeto = response.data
-lat  = objeto[0]['geoCode']['latitude']
-lon = objeto[0]['geoCode']['longitude']
-
-response = amadeus.safety.safety_rated_locations.get(latitude=(str)(lat), longitude= (str)(lon))
-print(response.data[0])
-
-print(lat)
-print(lon)'''
-
-# Flight Offers Price
-
-
-
 print("Bot started...")
 
 data = dict()
 
-
 def get_id(update):
-	return update.message.chat.id
-
+    return update.message.chat.id
 
 def create_fields(id):
-	global data
-	data[id] = {'pergunta' : 0, 'src': '', 'dest': '', 'contrycode' : '', 'adults' : '', 'data' : '', 'upsell' : '', 'Permanence' : False}
-
+    global data
+    data[id] = {'pergunta' : 0, 'src': '', 'dest': '', 'contrycode' : '', 'adults' : '', 'data' : '', 'upsell' : '', 'Permanence' : False}
 
 def start_command(update, context):
-	global data
-	update.message.reply_text("Seja bem vindo ao seu ajudante virtual. Acredito que você precise de ajuda para iniciar sua jornada como nômade! Digite 'go' para encontrar o voo que te levará a seu próximo destino!")
-	create_fields(get_id(update))
-
+    global data
+    update.message.reply_text("Seja bem vindo ao seu ajudante virtual. Acredito que você precise de ajuda para iniciar sua jornada como nômade! Digite 'go' para encontrar o voo que te levará a seu próximo destino!")
+    create_fields(get_id(update))
 
 def help_command(update, context):
-	update.message.reply_text("Então, por enquanto sou só um protótipo, realizo apenas o pré-cadastro das pessoas. Digite 'cadastro' se deseja iniciar o processo.")
-
+    update.message.reply_text("Então, por enquanto sou só um protótipo, realizo a simulação de compra de uma passagem aérea.")
 
 def handle_message(update, context):
-	text = str(update.message.text).lower()
-	response = r.sample_responses(text, data[get_id(update)])
-	update.message.reply_text(response)
-	print(update.message.chat.id)
-	print(data[get_id(update)]['pergunta'])
-	if data[get_id(update)]['pergunta'] == -1:
-		update.message.reply_text(json.dumps(data[get_id(update)]))
-		save_file(data, get_id(update))
-
+    text = str(update.message.text).lower()
+    response = r.sample_responses(text, data[get_id(update)])
+    update.message.reply_text(response)
+    print(update.message.chat.id)
+    print(data[get_id(update)]['pergunta'])
+    if data[get_id(update)]['pergunta'] == -1:
+        update.message.reply_text(json.dumps(data[get_id(update)]))
 
 def error(update, context):
-	print(f"Update {update} caused error {context.error}")
-
+    print(f"Update {update} caused error {context.error}")
 
 def main():
-	updater = Updater('5513016229:AAH99XIqBllKTdnV6RNEAKp3_kgAMemfBZI', use_context = True)
-	dp = updater.dispatcher
-	dp.add_handler(CommandHandler("start", start_command))
-	dp.add_handler(CommandHandler("help", help_command))
+    updater = Updater('5513016229:AAH99XIqBllKTdnV6RNEAKp3_kgAMemfBZI', use_context = True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start_command))
+    dp.add_handler(CommandHandler("help", help_command))
 
-	dp.add_handler(MessageHandler(Filters.text, handle_message))
-	dp.add_error_handler(error)
+    dp.add_handler(MessageHandler(Filters.text, handle_message))
+    dp.add_error_handler(error)
 
-	updater.start_polling()
-	updater.idle()
+    updater.start_polling()
+    updater.idle()
 
 main()
--- pythonProject/responses.py
+++ pythonProject/responses.py
@@ -1,6 +1,6 @@
 
-from telegram.ext import *
-from    main.py import * 
+#from telegram.ext import *
+#from    main.py import * 
 
 # Verificação de digitos
 def has_numbers(inputString):
@@ -49,73 +49,69 @@ amadeus = Client(
 # Caso esteja tudo ok, o usuário recebe a próxima pergunta, caso contrário,
 # uma mensagem de erro é exibida até que ele coloque uma resposta adequada.
 def sample_responses(input_text, data):
-	user_message = str(input_text).lower()
+    user_message = str(input_text).lower()
 
-
-	if "go" in user_message:
-		data['pergunta'] = 1
-		return "Perfeito! De que cidade você quer partir?"
-	elif data['pergunta'] == 1:
-		if has_numbers(user_message):
-			data['pergunta'] = 1
-			return "Nem eu que sou uma robô tenho números no meu nome. o__O Tente digitar o nome correto de sua cidade."
-		else:
-			data.update({'src':user_message})
-			data['pergunta'] += 1
-			return "Agora o nome da cidade de seu destino, mas sem pontos nem traços, por favor. :-)"
-	elif data['pergunta'] == 2:
-		if has_numbers(user_message):
-			data['pergunta'] = 2
-			return "Somente o nome da cidade, por favor."
-		else:
-			data.update({'dest': user_message})
-			data['pergunta'] += 1
-			return "Qual o código do seu país. Ex: Brasil -> BR, França -> FR, Estados Unidos -> US"
-	elif data['pergunta'] == 3:
-		if user_message in ("br, fr, us, pr, ca, it, pt, en"):
-			data.update({'contrycode': user_message})
-			data['pergunta'] += 1
-			return "É um lugar lindo! \*___\* Já estamos na metade! Quantos adultos irão nessa viagem contando com você?"
-		else:
-			data['pergunta'] == 3
-			return "Não reconheço esse código x__x Digite um código valido"
-	elif data['pergunta'] == 4:
-		if user_message.isdigit() is False:
-			data['pergunta'] = 4
-			return "Somente os dígitos, por favor. ;-)"
-		else:
-			data.update({'adults': user_message})
-			data['pergunta'] += 1
-			return "Muito bem, agora eu preciso saber que dia você espera estar partindo! Digite data no seguinte formato ano-mes-dia. ex:2022-12-25 (Então é natal, HO HO HO!)"
-	elif data['pergunta'] == 5:
-		if is_data(user_message) == 0:
-			data['pergunta'] = 5
-			return "Somente uma data válida será aceita. ;-)"
-		else:
-			data.update({'data': user_message})
-			data['pergunta'] += 1
-			
-			try:
-				response = amadeus.shopping.flight_offers_search.get(
-				originLocationCode=data['src'].upper(),
-				destinationLocationCode=data['dest'].upper(),
-				departureDate=data['data'].upper(),
-				adults=1)
-				update.message.reply_text(response.data['price'])
-			except ResponseError as error:
-				print(error)
-				return "Tem interesse em seguros de saúde? Responda com \"sim\" ou \"não\"."
-	elif data['pergunta'] == 6:
-			data.update({'upsell': user_message})
-			data['pergunta'] += 1
-			return "Para terminar! Você pretende ficar por mais de 6 meses? Responda com \"sim\" ou \"não\"."
-	elif data['pergunta'] == 7:
-		data['pergunta'] = -1
-		if user_message in ("sim", "s"):
-			data.update({'Permanence': True})
-		else:
-			data.update({'Permanence': False})
-			print(data)
-		return "Prontinho! Já terminamos! Obrigada pelas respostas. Agora vamos conectar você à instituição que mais combina com o seu perfil. Aguarde o retorno deles em alguns dias, beleza? Abraços, vou ficando por aqui! :-D"
-	if user_message in ("oi", "ola", "olá"):
-					return "Olááá! :-D"
+    if "go" in user_message:
+        data['pergunta'] = 1
+        return "Perfeito! De que cidade você quer partir?"
+    elif data['pergunta'] == 1:
+        if has_numbers(user_message):
+            data['pergunta'] = 1
+            return "Nem eu que sou uma robô tenho números no meu nome. o__O Tente digitar o nome correto de sua cidade."
+        else:
+            data.update({'src':user_message})
+            data['pergunta'] += 1
+            return "Agora o nome da cidade de seu destino, mas sem pontos nem traços, por favor. :-)"
+    elif data['pergunta'] == 2:
+        if has_numbers(user_message):
+            data['pergunta'] = 2
+            return "Somente o nome da cidade, por favor."
+        else:
+            data.update({'dest': user_message})
+            data['pergunta'] += 1
+            return "Qual o código do seu país. Ex: Brasil -> BR, França -> FR, Estados Unidos -> US"
+    elif data['pergunta'] == 3:
+        if user_message in ("br, fr, us, pr, ca, it, pt, en"):
+            data.update({'contrycode': user_message})
+            data['pergunta'] += 1
+            return "É um lugar lindo! \*___\* Já estamos na metade! Quantos adultos irão nessa viagem contando com você?"
+        else:
+            data['pergunta'] == 3
+            return "Não reconheço esse código x__x Digite um código valido"
+    elif data['pergunta'] == 4:
+        if user_message.isdigit() is False:
+            data['pergunta'] = 4
+            return "Somente os dígitos, por favor. ;-)"
+        else:
+            data.update({'adults': user_message})
+            data['pergunta'] += 1
+            return "Muito bem, agora eu preciso saber que dia você espera estar partindo! Digite data no seguinte formato ano-mes-dia. ex:2022-12-25 (Então é natal, HO HO HO!)"
+    elif data['pergunta'] == 5:
+        if is_data(user_message) == 0:
+            data['pergunta'] = 5
+            return "Somente uma data válida será aceita. ;-)"
+        else:
+            data.update({'data': user_message})
+            data['pergunta'] += 1
+        try:
+            response = amadeus.shopping.flight_offers_search.get(
+            originLocationCode=data['src'].upper(),
+            destinationLocationCode=data['dest'].upper(),
+            departureDate=data['data'].upper(),
+            adults=1)
+        except ResponseError as error:
+            print(error) 
+        price = float(response.data[0]['price']['grandTotal']) * 5.57
+        return ("O preço da passagem ficou em " + str(price) + " Reais.\nTem interesse em seguros de saúde? Responda com \"sim\" ou \"não\".")
+    elif data['pergunta'] == 6:
+        data.update({'upsell': user_message})
+        data['pergunta'] += 1
+        return "Para terminar! Você pretende ficar por mais de 6 meses? Responda com \"sim\" ou \"não\"."
+    elif data['pergunta'] == 7:
+        data['pergunta'] = -1
+        if user_message in ("sim", "s"):
+            data.update({'Permanence': True})
+        else:
+            data.update({'Permanence': False})
+            print(data)
+        return "Prontinho! Já terminamos! Obrigada pelas respostas. Aguarde o retorno de seu pedido em seu email :-D"
