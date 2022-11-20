f110ea203e57c7e93a304146bc17d6f0bf307ed6
Vitor Santanna <95882160+vitorsantanna2@users.noreply.github.com>
2022-11-20 18:53:51 -0300

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -5,8 +5,8 @@ import responses as r
 from amadeus import Client, ResponseError, Location
 
 amadeus = Client(
-    client_id='oGDkd7G61c0KfkmVV3oAQtMNryvoW6VG',
-    client_secret='GwXAbyPUziKZC8S9'
+    client_id='API KEY',
+    client_secret='API KEY SECRET'
 )
 
 print("Bot started...")
@@ -39,7 +39,7 @@ def error(update, context):
     print(f"Update {update} caused error {context.error}")
 
 def main():
-    updater = Updater('5513016229:AAH99XIqBllKTdnV6RNEAKp3_kgAMemfBZI', use_context = True)
+    updater = Updater('API KEY', use_context = True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start_command))
     dp.add_handler(CommandHandler("help", help_command))
